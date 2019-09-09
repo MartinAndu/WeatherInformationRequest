c3741d3826e527bd5a064c453c489a560358af87
Martin Andujar <martin@easytech.com.ar>
2019-09-09 18:47:42 -0300

    Config SpringBootServletInitializer for heroku deployment

--- src/main/java/com/challenge/SolarSystemApplication.java
+++ src/main/java/com/challenge/SolarSystemApplication.java
@@ -3,9 +3,10 @@ package com.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SolarSystemApplication {
+public class SolarSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SolarSystemApplication.class, args);
