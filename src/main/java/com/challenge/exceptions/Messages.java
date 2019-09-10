223af5baa46d85628c9b12acec8dd0483f2cec93
Martin Andujar <martin@easytech.com.ar>
2019-09-10 05:56:26 -0300

    Fix error message

--- src/main/java/com/challenge/exceptions/Messages.java
+++ src/main/java/com/challenge/exceptions/Messages.java
@@ -4,6 +4,6 @@ import com.challenge.model.consts.Consts;
 
 public class Messages {
     public static final String OUT_OF_BOUNDS_PLANETS = "Planets constraint allows only three planets maximum";
-    public static final String DAY_NUMBER_EXCEPTION = "Day input format is incorrect. It must be defined in a range between 0 and " + Consts.AMOUNT_OF_DAYS;
+    public static final String DAY_NUMBER_EXCEPTION = "Day input format is incorrect. It must be defined in a range between 0 and " + (Consts.AMOUNT_OF_DAYS - 1);
     public static final String NUMBER_FORMAT_EXCEPTION = "The number is not a valid format, it should be an integer number";
 }
--- src/main/java/com/challenge/exceptions/validators/DayValidator.java
+++ src/main/java/com/challenge/exceptions/validators/DayValidator.java
@@ -4,6 +4,6 @@ import com.challenge.model.consts.Consts;
 
 public class DayValidator {
     public static boolean  ValidateDay(int day) {
-        return day >=0 && day <= Consts.AMOUNT_OF_DAYS;
+        return day >=0 && day < Consts.AMOUNT_OF_DAYS;
     }
 }
